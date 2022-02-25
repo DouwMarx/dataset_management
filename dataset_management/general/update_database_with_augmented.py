d14ef0f1b8bfa5af18f67ab070673857480d7f85
Douw Marx <douwmarx@gmail.com>
2022-02-25 18:08:18 +0100

    Augmentation is temporarily based on actual failure data magnitude

--- dataset_management/general/update_database_with_augmented.py
+++ dataset_management/general/update_database_with_augmented.py
@@ -2,6 +2,7 @@ import pickle
 from augment_data.phenomenological_ses.make_phenomenological_ses import AugmentedSES
 from database_definitions import make_db
 from dataset_management.ultils.update_database import DerivedDoc, new_docs_from_computed
+import numpy as np
 
 
 def compute_augmentation_from_feature_doc(doc):
@@ -30,21 +31,33 @@ def compute_augmentation_from_feature_doc(doc):
                                                               "mode"]})
 
     healthy_envelope_spectrum = pickle.loads(healthy_envelope_spectrum["envelope_spectrum"])
-    healthy_envelope_spectrum = healthy_envelope_spectrum["mag"]
+    healthy_envelope_spectrum_mag = healthy_envelope_spectrum["mag"]
+    healthy_envelope_spectrum_freq = healthy_envelope_spectrum["freq"]
 
     meta_data = pickle.loads(doc["meta_data"])
     fs = meta_data["sampling_frequency"]
 
     expected_fault_frequency = meta_data["derived"]["average_fault_frequency"]
 
-    # # Using all of the healthy ses as input means that the augmented dataset will have the noise of the training set
-    # # However, among the augmented dataset the "signal" will be identical
-    # # notice the "0" meaning that we are using healthy data
-    # healthy_ses = pickle.loads(entry["envelope_spectrum"])["mag"]  # [0] # Use the first one
+    # TODO: Very important, Notice that setting the augmented amplitude using failure data is cheating: This is however now done for rapid iteration.
+    # However, there will always be a disrepancy in practice.
+    max_severity = db["processed"].distinct("severity")[-1]
+    maximal_damage_for_mode = db["processed"].find_one({"envelope_spectrum": {"$exists": True},
+                                                          "severity": max_severity,
+                                                          "mode": doc["mode"]})
+    damaged_envelope_spectrum = pickle.loads(maximal_damage_for_mode["envelope_spectrum"])["mag"]
+    max_amplitude = np.max(damaged_envelope_spectrum)
 
-    ases = AugmentedSES(healthy_ses=healthy_envelope_spectrum, fs=fs, fault_frequency=expected_fault_frequency,
-                        peak_magnitude=0.01,
-                        percentage_of_freqs_to_decay_99_percent=0.5)  # TODO: Fix peak magnitude, providing augmentation parameters?
+    print("mode",doc["mode"],"max amp",max_amplitude)
+
+
+    ases = AugmentedSES(healthy_ses=healthy_envelope_spectrum_mag,
+                        healthy_ses_freq= healthy_envelope_spectrum_freq,
+                        fs=fs,
+                        fault_frequency=expected_fault_frequency,
+                        peak_magnitude=max_amplitude,#0.01,
+                        decay_percentage_over_interval=0.5
+                        )  # TODO: Fix peak magnitude, providing augmentation parameters?
 
     envelope_spectrum = ases.get_augmented_ses()
 
