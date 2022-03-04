f51329994c659878eb28b100f1687651a96b5aca
Douw Marx <douwmarx@gmail.com>
2022-03-04 11:27:33 +0100

    phenomenological bearing data can be added to datababse, sample by sample

--- dataset_management/phenomenological_model/add_raw_data_to_database.py
+++ dataset_management/phenomenological_model/add_raw_data_to_database.py
@@ -1,8 +1,10 @@
 import pickle
 from pypm.phenomenological_bearing_model.make_data import PyBearingDataset
+from tqdm import tqdm
+
 from database_definitions import make_db
 
-def build_phenomenological_database(db_to_act_on, n_severities = 10, rapid=True):
+def build_phenomenological_database(db_to_act_on, n_severities = 3, rapid=True):
     # Mongo database
     db,client = make_db(db_to_act_on)
     db["raw"].delete_many({}) #  Remove the items in the collection
@@ -12,27 +14,32 @@ def build_phenomenological_database(db_to_act_on, n_severities = 10, rapid=True)
 
     # Pack the bearing dataset into the database
     for mode_name,mode_data in result_dict.items():
-        for severity_name,severity_data in mode_data.items():
-
+        print(mode_name)
+        docs_for_mode = []
+        for severity_name,severity_data in tqdm(mode_data.items()):
             meta_data = severity_data["meta_data"]
             time_series = severity_data["time_domain"]
 
-            doc = {"mode": mode_name,
-                   "severity": severity_name,
-                   # "meta_data": pickle.dumps(meta_data),
-                   "meta_data": meta_data,
-                   "time_series": pickle.dumps(time_series),
-                   "augmented":False
-                   }
+            for signal in time_series: # Each row is a signal
+                doc = {"mode": mode_name,
+                       "severity": severity_name,
+                       "meta_data": meta_data,
+                       "time_series": list(signal),
+                       "augmented":False
+                       }
 
-            db["raw"].insert_one(doc)  # Insert document into the collection
+                docs_for_mode.append(doc)
+
+        db["raw"].insert_many(docs_for_mode)  # Insert document into the collection
+
+    print(db["raw"].count_documents({}))
 
     return db["raw"]# The mongodb collection
 
 
 def main():
     db_to_act_on = "phenomenological_rapid"
-    sevs = 3
+    sevs = 10
     fd = build_phenomenological_database(db_to_act_on,n_severities=sevs, rapid=False)
     r = fd.find({"mode":"inner"})
     l = len(list(r)) # Check that the length of the database will be the same as the number of severities
