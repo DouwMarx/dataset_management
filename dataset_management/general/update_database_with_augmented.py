2a9a4d6ce530b0cb20b31e43ef2ed9f362820d03
Douw Marx <douwmarx@gmail.com>
2022-04-04 14:23:32 +0200

    Augmentation pipline running with new traingle augmentation with more elegant equations

--- dataset_management/general/update_database_with_augmented.py
+++ dataset_management/general/update_database_with_augmented.py
@@ -1,6 +1,5 @@
-import pickle
-from augment_data.phenomenological_ses.make_phenomenological_ses import AugmentedSES
-from augment_data.envelope_with_traingular_spikes.make_envelope_spectrum_triangular import AugmentedEnvelopeFromFaultFrequencies
+from augment_data.envelope_with_traingular_spikes.make_envelope_spectrum_triangular import \
+    AugmentedEnvelopeFromFaultFrequencies,AugmentedEnvelopeImprovedTriangular
 from database_definitions import make_db
 from dataset_management.ultils.update_database import DerivedDoc, new_docs_from_computed
 import numpy as np
@@ -23,10 +22,10 @@ class Augmentation():
             docs = self.db["processed"].find(
                 {"envelope_spectrum": {"$exists": True},
                  "mode": mode
-                 }).sort("severity", direction=-1).limit(20) # Use the final 20 documents to estimate the maximum severity
+                 }).sort("severity", direction=-1).limit(
+                20)  # Use the final 20 documents to estimate the maximum severity
             env_spec_max = [np.max(doc["envelope_spectrum"]["mag"]) for doc in docs]
-            self.env_spec_max_for_mode.update({mode:np.median(env_spec_max)})
-
+            self.env_spec_max_for_mode.update({mode: np.median(env_spec_max)})
 
     def compute_augmentation_from_healthy_feature_doc(self, doc):
         """
@@ -56,48 +55,43 @@ class Augmentation():
 
         expected_fault_frequency_for_mode = meta_data["expected_fault_frequencies"]
 
+        # Instantiate the triagle at frequency augmentation
+
         augmented_doc_for_mode = []
         # Loop through the different failure modes that could appear
+        ases = AugmentedEnvelopeImprovedTriangular(healthy_envelope_spectrum_freq,)
         for fault_mode in ["ball", "inner", "outer"]:
             expected_fault_frequency = expected_fault_frequency_for_mode[fault_mode]
 
             if fault_mode in self.env_spec_max_for_mode:
-                peak_mag = self.env_spec_max_for_mode[fault_mode] # TODO: notice that knowing the expected magnitude is cheating.
+                peak_mag = self.env_spec_max_for_mode[
+                    fault_mode]  # TODO: notice that knowing the expected magnitude is cheating.
             else:
                 peak_mag = self.env_spec_max_for_mode[max(self.env_spec_max_for_mode)]
 
-            # ases = AugmentedSES(healthy_ses=healthy_envelope_spectrum_mag,
-            #                     healthy_ses_freq=healthy_envelope_spectrum_freq,
-            #                     fs=fs,
-            #                     fault_frequency=expected_fault_frequency,
-            #                     # peak_magnitude=0.04,  # max_amplitude,#
-            #                     peak_magnitude= peak_mag,  # max_amplitude,#
-            #                     decay_percentage_over_interval=0.5
-            #                     )  # TODO: Fix peak magnitude, providing augmentation parameters?
+            # ases = AugmentedEnvelopeFromFaultFrequencies(healthy_ses=healthy_envelope_spectrum_mag,
+            #                                              healthy_ses_freq=healthy_envelope_spectrum_freq,
+            #                                              fs=fs,
+            #                                              fault_frequency=expected_fault_frequency,
+            #                                              # peak_magnitude=0.04,  # max_amplitude,#
+            #                                              peak_magnitude=peak_mag,  # max_amplitude,#
+            #                                              decay_percentage_over_interval=0.999
+            #                                              )  # TODO: Fix peak magnitude, providing augmentation parameters?
 
-            ases = AugmentedEnvelopeFromFaultFrequencies(healthy_ses=healthy_envelope_spectrum_mag,
-                                healthy_ses_freq=healthy_envelope_spectrum_freq,
-                                fs=fs,
-                                fault_frequency=expected_fault_frequency,
-                                # peak_magnitude=0.04,  # max_amplitude,#
-                                peak_magnitude= peak_mag,  # max_amplitude,#
-                                decay_percentage_over_interval=0.999
-                                )  # TODO: Fix peak magnitude, providing augmentation parameters?
-
-            augmented_envelope_spectrum = ases.get_augmented_ses()
+            augmented_envelope_spectrum = ases.get_augmented_ses(healthy_envelope_spectrum_mag,peak_mag,expected_fault_frequency)
 
             computed = {"envelope_spectrum": {"freq": list(ases.frequencies),
                                               "mag": list(augmented_envelope_spectrum)},
                         "augmented": True,
                         "meta_data": ases.augmentation_meta_data,
-                        "mode":fault_mode,
-                        "severity":999} # Use 999 severity for now
+                        "mode": fault_mode,
+                        "severity": 999}  # Use 999 severity for now
 
             augmented_doc_for_mode.append(computed)
 
-
         new_docs = new_docs_from_computed(doc, augmented_doc_for_mode)  # Add the meta-data keys
         return new_docs
+
 
 def ims_outer_t2_c1_aug(db_to_act_on):
     db, client = make_db(db_to_act_on)
@@ -108,13 +102,14 @@ def ims_outer_t2_c1_aug(db_to_act_on):
     # Compute augmented data
     query = {"envelope_spectrum": {"$exists": True},
              "severity": 0,
-            'ims_test_number': "2",
-            'ims_channel_number': "1",
-        }
+             'ims_test_number': "2",
+             'ims_channel_number': "1",
+             }
     DerivedDoc(query, "processed", "augmented", aug_obj.compute_augmentation_from_healthy_feature_doc,
                db_to_act_on).update_database(parallel=False)
 
     return db["augmented"]
+
 
 def main(db_to_act_on):
     db, client = make_db(db_to_act_on)
@@ -129,6 +124,6 @@ def main(db_to_act_on):
 
     return db["augmented"]
 
-if __name__ == "__main__":
-    r = main("phenomenological_rapid")
 
+if __name__ == "__main__":
+    r = main("phenomenological")
