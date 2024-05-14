f22fb9814fe591db331cd271d4419b4c108e0b32
Douw Marx <douw.marx@kuleuven.be>
2024-05-14 11:17:17 +0200

    Add siemens nvh dataset

--- dataset_management/safran_simulated_gearbox_and_bearing/write_to_standard_structure.py
+++ dataset_management/safran_simulated_gearbox_and_bearing/write_to_standard_structure.py
@@ -3,27 +3,29 @@ import scipy
 from dataset_management.ultils.write_data_in_standard_format import export_data_to_file_structure
 from file_definitions import biased_anomaly_detection_path
 
-healthy_data = "/home/douwm/data/safran_simulated_gearbox_and_bearing/data/simple_healty_dataset.mat"
-faulty_data = "/home/douwm/data/safran_simulated_gearbox_and_bearing/data/simple_faulty_dataset.mat"
+for difficulty_level in ["simple", "moderate", "extreme"]:
+    healthy_data = "/home/douwm/data/safran_simulated_gearbox_and_bearing/data/simple_healthy_dataset.mat"
+    faulty_data = "/home/douwm/data/safran_simulated_gearbox_and_bearing/data/simple_faulty_dataset.mat"
 
-healthy_data = scipy.io.loadmat(healthy_data)
-faulty_data = scipy.io.loadmat(faulty_data)
+    healthy_data = scipy.io.loadmat(healthy_data)
+    faulty_data = scipy.io.loadmat(faulty_data)
 
-Fs = healthy_data['Fs'][0, 0]
+    Fs = healthy_data['Fs'][0, 0]
 
-# Retrieve healthy and faulty data signals
-x0 = healthy_data['data'][0]
-x = faulty_data['data'][0]
+    # Retrieve healthy and faulty data signals
+    x0 = healthy_data['data'][0]
+    x = faulty_data['data'][0]
 
-# Find the number of samples required to ensure a sufficient number of fault events per segment
-n_faut_events_per_segement = 10
-n_samples_required_to_capture_fault_events = n_faut_events_per_segement * healthy_data['Event'][0, 0]
-resulting_number_of_segments = len(x0) // n_samples_required_to_capture_fault_events
+    # Find the number of samples required to ensure a sufficient number of fault events per segment
+    n_faut_events_per_segement = 10
+    n_samples_required_to_capture_fault_events = n_faut_events_per_segement * healthy_data['Event'][0, 0]
+    resulting_number_of_segments = len(x0) // n_samples_required_to_capture_fault_events
 
-
-# Export to standard file structure
-metadata = { "sampling_frequency": int(Fs),
-                "n_faut_events_per_segement": int(n_faut_events_per_segement),
-                "n_samples_required_to_capture_fault_events": int(n_samples_required_to_capture_fault_events),
-                "resulting_number_of_segments": int(resulting_number_of_segments)}
-export_data_to_file_structure("safran_simple",x0,{"faulty":x}, export_path=biased_anomaly_detection_path,metadata=metadata)
+    # Export to standard file structure
+    metadata = { "sampling_frequency": int(Fs),
+                    "n_faut_events_per_segement": int(n_faut_events_per_segement),
+                    "n_samples_required_to_capture_fault_events": int(n_samples_required_to_capture_fault_events),
+                    "resulting_number_of_segments": int(resulting_number_of_segments),
+                  "level_of_difficulty": difficulty_level
+                 }
+    export_data_to_file_structure("safran_" + difficulty_level, x0, {"faulty":x}, export_path=biased_anomaly_detection_path, metadata=metadata)
